d4f2f678a902c24939355285718933fc7e99bf05
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2023-03-15 15:15:56 +0000

    Add path to missing python elements\nIncludes the path to missing python librarires as well as a script for discovering the tests.

---
+++ python/pyFSD/ctesthelper.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def _print_suite(test_suite_or_iterable):
+    if not hasattr(test_suite_or_iterable, '__iter__'):
+        print(test_suite_or_iterable.id())
+    else:
+        for item in test_suite_or_iterable:
+            _print_suite(item)
+
+
+def discover_tests():
+    _print_suite(unittest.defaultTestLoader.discover('.'))
